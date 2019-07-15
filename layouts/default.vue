02e0fd7867bb75715111b72114f3b93b262d766b
Syifandi Mulyanto <fanfandi17@gmail.com>
2019-07-15 14:53:36 +0700

    header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,28 @@
 <template>
   <div>
+    <header>
+      <div class="container">
+        <div class="logo">
+          <a href="#"><img src="~assets/image/icon.png"></a>
+          <div class="title">
+            <h4>Foundation</h4>
+            <p>Startup Landing Template</p>
+          </div>
+        </div>
+        <div class="menu">
+          <ul> 
+            <li class="active">
+              <a href="#" class="sub-menu">Home</a>
+            </li>
+            <li><a href="#" class="sub-menu">Stories</a></li>
+            <li><a href="#" class="sub-menu with-dropdown">Libraries</a></li>
+            <li>
+              <a href="#" class="btn primary get-started">Get Started</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
     <nuxt />
   </div>
 </template>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
