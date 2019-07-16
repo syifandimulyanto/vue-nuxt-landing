15f025faa27bc567056e030c1ed606bcc50c71a4
Syifandi Mulyanto <fanfandi17@gmail.com>
2019-07-16 19:40:42 +0700

    landing all section

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,313 @@
           </div>
       </div>
     </section>
+    <section class="section-home-second">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="content">
+              <h3>What is it about?</h3>
+              <p>Building a website for a startup that looks amazing is not a rocket science anymore.</p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+              <h3>Who is it for?</h3>
+              <p>Startups, small companies and teams, entrepreneurs and web developers.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-home-third">
+      <div class="container">
+        <div class="columns sponsorship-image">
+          <div class="column">
+            <img src="~assets/image/tnt.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/forbes.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/apple.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/mashable.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/wsj.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/google.png">
+          </div>
+        </div>
+        <div class="feature">
+          <div class="columns">
+            <div class="column">
+              <a href="" class="small-title">Features</a>
+              <h1>Easily find leads and customers</h1>
+              <p>Send one-off and automated email, push, and in-app messages to people. Create better stories.</p>
+              <div class="acquire-section">
+                <img class="look" src="~assets/image/look.png">
+                <h3>Acquire new customers</h3>
+                <p>Everything you need to start building – including open-source code, documentation.</p>
+                <ul>
+                  <li><img src="~assets/image/engage.png"> Engage users </li>
+                  <li><img src="~assets/image/across.png"> Develop across platforms</li>
+                </ul>
+              </div>
+            </div>
+            <div class="column bg-image">
+              <img src="~assets/image/invision.png">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-home-four">
+      <div class="container">
+        <div class="columns">
+          <div class="column left-content">
+            <div class="timeline">
+              <img src="~assets/image/timeline.png">
+            </div>
+            <div class="timeline">
+              <p>Connect with customers and grow faster</p>
+            </div>
+          </div>
+          <div class="column right-content">
+            <a href="#" class="team-inbox">Team Inbox</a>
+            <h1 class="conversation">Manage <br> conversations</h1>
+            <p class="conversation-detail">One place to manage and respond to all conversations with leads and users. Receive messages from leads.</p>
+            <div class="box-card">
+              <p>“Great widgets. Great selection. Great design and <b>easy to implement</b>. Definitely a huge time saver for a web developer!”</p>
+              <div class="profile">
+                <img src="~assets/image/vanila.png"> 
+                <div class="profile-description">
+                  <h3>Viella Malkovich</h3> 
+                  <small>Creative Director at Johnson</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-home-five">
+      <div class="container">
+        <div class="columns">
+          <div class="column left-content">
+            <a href="#" class="communicate">Communicate Better</a>
+            <h1>Built for busy small teams</h1>
+            <p>Three products that can be used independently or combined together for your company’s needs.</p>
+            <a href="#" class="btn primary get-started">Get Started</a>
+          </div>
+          <div class="column right-content">
+            <div class="better">
+              <img src="~assets/image/phone.png">
+              <p>A better way to acquire new users</p>
+            </div>
+          </div>
+        </div>
+        <div class="columns content-row-description">
+          <div class="column content">
+            <p><b>Subscriptions.</b> Foundation works with the tools and services you already use every day.</p>
+            <a href="">Learn more about subscriptions</a>
+          </div>
+          <div class="column content">
+            <p><b>Felxible Hours.</b> Get started with our simple snippet of JavaScript or easy to install SDKs for iOS and Android.</p>
+            <a href="">Learn more about subscriptions</a>
+          </div>
+          <div class="column content">
+            <p><b>Fast Communication.</b> We offer measures like 2FA and SSO to ensure the safety of your data.</p>
+            <a href="">Learn more about subscriptions</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-home-six">
+      <div class="head-title">
+        <h3>Engage Visitors</h3>
+        <h1>Understand your <br>audience</h1>
+        <p>Early stage company? Eligible applicants get all of our products for just $49 a month.</p>
+        <div class="learn-or-started">
+          <a href="#" class="btn primary get-started">Get Started </a> &nbsp;&nbsp;
+          <a href="#" class="btn info learn-more">Learn More</a>
+        </div>
+      </div>
+      <div class="columns card-content">
+        <div class="column">
+          <div class="card">
+            <img src="~assets/image/developer.png">
+            <h3>Developers</h3>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="~assets/image/tv.png">
+            <h3>Entertainment</h3>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="~assets/image/run.png">
+            <h3>Athletes</h3>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="~assets/image/creator.png">
+            <h3>Creators</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-home-seven">
+      <div class="container">
+        <div class="columns sponsorship-image">
+          <div class="column">
+            <img src="~assets/image/tnt.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/forbes.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/apple.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/mashable.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/wsj.png">
+          </div>
+          <div class="column">
+            <img src="~assets/image/google.png">
+          </div>
+        </div>
+        <div class="head">
+          <h3>Trusted by the world’s most innovative businesses – big and small</h3>
+          <p>“Comprehensive set of startup tools for all imaginable entrepreneurial needs. Create landing pages, send emails, find freelancers. Perfect for sales, marketing, and support”</p>
+          <img class="import" src="~assets/image/import.png">
+        </div>
+      </div>
+    </section>
+    <section class="section-home-eight">
+      <div class="hiring">
+        <div class="head">
+          <h1>We’re hiring</h1>
+          <p>We’re a team of lifelong learners. We’re equal parts left and right brained.</p>
+          <a class="btn primary">More about Company</a>
+        </div>
+        <div class="career-position">
+          <div class="list">
+            <div class="position">
+              <h3>Front-End Developer</h3>
+              <small>Los Angeles / Remote</small>
+            </div>
+          </div>
+          <div class="list">
+            <div class="position">
+              <h3>Community Manager</h3>
+              <small>New York / Full-Time</small>
+            </div>
+          </div>
+          <div class="list">
+            <div class="position">
+              <h3>UX/UI Designer</h3>
+              <small>New York / Full-Time</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-home-nine">
+      <div class="columns">
+        <div class="column left-content">
+          <img src="~assets/image/dog.png">
+        </div>
+        <div class="column is-three-fifths right-content">
+          <div class="content">
+            <h1>Start your free trial.</h1>
+            <p>Get notified about company updates, news and blog posts. We hate spam.</p>
+            <div class="form">
+              <form class="">
+                <input type="text" placeholder="Enter your email">
+                <button class="btn primary get-started" type="submit">Get Started</button>
+                <div class="description">
+                  <small>Free 14-day trial · Easy setup · Cancel any time. Check out </small>
+                  <a href="">Terms of Use.</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-footer">
+      <div class="columns">
+        <div class="column left-content">
+          <div class="logo">
+            <a href="#"><img src="~assets/image/icon.png"></a>
+            <div class="title">
+              <h4>Foundation</h4>
+              <p>Startup Landing Template</p>
+            </div>
+            <div class="description">
+              <p>Foundation is a website template for startups and small teams. It helps to build a website in no time.</p>
+            </div>
+            <div class="download">
+              <img src="~assets/image/appstore.png">
+              <img src="~assets/image/googleplay.png">
+            </div>
+          </div>
+        </div>
+        <div class="column is-three-fifths right-content">
+          <div class="columns">
+            <div class="column">
+              <div class="menu">
+                <h1>Product</h1>
+                <ul>
+                  <li><a href="#">Acquire Users</a></li>
+                  <li><a href="#">Content Marketing</a></li>
+                  <li><a href="#">Website Templates</a></li>
+                  <li><a href="#">Customer Management</a></li>
+                  <li><a href="#">Virtual Inbox</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="column">
+              <div class="menu">
+                <h1>Company</h1>
+                <ul>
+                  <li><a href="#">About Foundation</a></li>
+                  <li><a href="#">Brand Guidelines</a></li>
+                  <li><a href="#">Press Kit</a></li>
+                  <li><a href="#">Support</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns end-footer">
+        <div class="column left-content">
+          <ul>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">License</a></li>
+          </ul>
+        </div>
+        <div class="column right-content">
+          <div class="is-pulled-right">
+            <ul>
+              <li><a href="#"><img src="~assets/image/icon-fb.png"></a></li>
+              <li><a href="#"><img src="~assets/image/icon-twitter.png"></a></li>
+              <li><a href="#"><img src="~assets/image/icon-linkedIn.png"></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
